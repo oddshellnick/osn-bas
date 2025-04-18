a16855f832889e5974ed72e75cbed22fa1af337a
Eduard Dik <oddshellnick.programming@gmail.com>
2025-04-18 13:35:40 +0700

    2.1.2-beta.1 release commit

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_description() -> str:
 
 setup(
 		name="osn-bas",
-		version="2.1.2-beta.0",
+		version="2.1.2-beta.1",
 		author="oddshellnick",
 		author_email="oddshellnick.programming@gmail.com",
 		description=get_description(),
